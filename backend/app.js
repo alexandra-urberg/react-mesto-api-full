69c9d1e555e3a7f51aff9f9ad146fbe6c65d7780
Sasha Stepanova <chernishevskie@yandex.ru>
2021-08-24 15:31:52 -0300

    feat: added logger to the backend

---
+++ backend/app.js
@@ -0,0 +1,71 @@
+require('dotenv').config(); // для секретного ключа
+const express = require('express');
+const cookieParser = require('cookie-parser'); // для кукис
+const rateLimit = require('express-rate-limit'); // защиты от DDoS-атак
+const mongoose = require('mongoose');
+const helmet = require('helmet'); // помогает защитить экспресс-приложения, устанавливая различные HTTP заголовки
+const { celebrate, Joi, errors } = require('celebrate'); // валидация роутинга
+const { createUser, login } = require('./controllers/users');
+const auth = require('./middlewares/auth');
+const errorsHandler = require('./middlewares/errors'); // для вывовода ошибок
+const NotFoundError = require('./errors/NotFoundError');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+
+const { PORT = 3000 } = process.env;
+const app = express();
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
+});
+
+app.use(helmet());
+app.use(cookieParser());
+app.use(limiter);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+});
+
+app.post('/signup', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    // eslint-disable-next-line
+    avatar: Joi.string().pattern(new RegExp('/^(https?|chrome):\/\/[^\s$.?#].[^\s]*$/gm')),
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+  }),
+}),
+createUser);
+
+app.use(requestLogger);
+
+app.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+  }),
+}),
+login);
+
+app.use('/', auth, require('./routes/users'));
+app.use('/', auth, require('./routes/cards'));
+
+app.use('*', (req, res, next) => {
+  next(new NotFoundError('Запрашиваемый адрес не найден'));
+});
+
+app.use(errorLogger);
+
+app.use(errors());
+app.use(errorsHandler);
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});

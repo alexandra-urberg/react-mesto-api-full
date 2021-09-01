fe6317b9da0603db3d9820e66f5aafc7b4f9fc11
Sasha Stepanova <chernishevskie@yandex.ru>
2021-09-01 14:06:27 -0300

    feat:(15-1) added default code 500

--- backend/middlewares/errors.js
+++ backend/middlewares/errors.js
@@ -1,6 +1,6 @@
 const errorsHandler = (err, req, res, next) => {
-  const status = err.statusCode;
-  const response = err.message;
+  const status = err.statusCode || 500;
+  const response = `Ошибка: ${err.message || 'Ошибка на сервере'}`;
 
   res.status(status).send({ response });
 
